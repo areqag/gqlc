dadb048c5ed83e2621a246a8195ad76c4e313481
antranig-yeretzian <antranig.yeretzian@proton.me>
2026-05-23 16:09:02 -0400

    put schema parser in correct directory with a clean interface

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"context"
 	"log/slog"
+	"os"
 
-	"github.com/antranig-yeretzian/gqlc/internal/grammar/gql"
+	schema_parser_gql "github.com/antranig-yeretzian/gqlc/internal/schema/gql"
 )
 
 func main() {
 	ctx := context.Background()
 
-	v := gql.NewSchemaVisitor("./test/data/sample_schema.gql")
-	if err := v.Parse(); err != nil {
+	f, err := os.Open("./test/data/sample_schema.gql")
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to open schema", "err", err)
+		return
+	}
+	defer f.Close()
+
+	p := schema_parser_gql.New()
+	if _, err := p.Parse(f); err != nil {
 		slog.ErrorContext(ctx, "failed to parse schema", "err", err)
 	}
 }
